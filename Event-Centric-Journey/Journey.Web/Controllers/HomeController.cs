08dc23edb77f0b92e14f378595250ea1e18da15d
Alexis Narváez <narvalex@hotmail.com>
2015-03-31 11:44:24 -0400

    Ahora funciona mi gravatar

--- Event-Centric-Journey/Journey.Web/Controllers/HomeController.cs
+++ Event-Centric-Journey/Journey.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Journey.Web.Controllers
 {
@@ -23,7 +19,6 @@ namespace Journey.Web.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
